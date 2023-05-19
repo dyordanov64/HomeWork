9ec39e599bd216d1a8525dc6aa91b75cccf0f917
dyordanov64 <d.yordanov64@abv.bg>
2023-05-19 09:16:50 +0300

    task-4

---
+++ HW Lab 15/Task_8.py
@@ -0,0 +1,19 @@
+# Задача 8. Дефинирайте клас, който съдържа информация за мобилен телефон: модел,
+# производител, цена, собственик, характеристики на батерията (модел, idle time и часове
+# разговор /hours talk/) и характеристики на екрана (големина и цветове).
+
+class Mobilen_Telefon
+    model = ''
+    manufacturer = ''
+    price = 0
+    owner = ''
+
+
+class Battery
+    model = ''
+    idle_time = 0
+    hours_talk = 0
+
+class Screen
+    size = 0
+    color = 0
---
+++ HW lab18- 10 05 2023/task-4.py
@@ -0,0 +1,20 @@
+# Задача 4. Използвайки файла Books.csv, помолете потребителя да въведе начална и
+# крайна година. Покажете всички книги, издадени между тези две години.
+
+import csv
+
+def search_book_of_years(file_name, year1, year2) :
+    have_book = False
+    with open(file_name, mode='+r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            if row[2] >= year1 and row[2]<=year2:
+                have_book = True
+                print(row[0])
+        else:
+            if not (have_book):
+                print(f'Няма налични книги този диапазон от години {year1} - {year2}')
+
+year1 = input('Въведете начална година:')
+year2 = input('Въведете крайна година:')
+search_book_of_years('Books.csv', year1, year2)
